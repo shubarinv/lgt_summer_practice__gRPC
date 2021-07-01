ea96db2f19904b5fa8f68cf2a3cdc7c2d842922e
Vladimir Shubarin <vhundef@pm.me>
2021-07-01 10:30:05 +0300

    Added FIleTransferService to gRPC(Server)

---
+++ grpc_server/Services/FIleTransferService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Google.Protobuf;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using ShubarinSummerPracticeGRPC;
+
+namespace grpc_server
+{
+    public class TransferService : FileTransferService.FileTransferServiceBase
+    {
+        private readonly ILogger<TransferService> _logger;
+
+        public TransferService(ILogger<TransferService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task DownloadFile(FileRequest request, IServerStreamWriter<ChunkMsg> responseStream,
+            ServerCallContext context)
+        {
+            var filePath = request.FilePath;
+
+            if (File.Exists(filePath))
+            {
+                var fileInfo = new FileInfo(filePath);
+
+                var chunk = new ChunkMsg
+                {
+                    FileName = Path.GetFileName(filePath),
+                    FileSize = fileInfo.Length
+                };
+
+                const int chunkSize = 64 * 1024;
+
+                var fileBytes = File.ReadAllBytes(filePath);
+                var fileChunk = new byte[chunkSize];
+
+                var offset = 0;
+
+                while (offset < fileBytes.Length)
+                {
+                    if (context.CancellationToken.IsCancellationRequested)
+                        break;
+
+                    var length = Math.Min(chunkSize, fileBytes.Length - offset);
+                    Buffer.BlockCopy(fileBytes, offset, fileChunk, 0, length);
+
+                    offset += length;
+
+                    chunk.ChunkSize = length;
+                    chunk.Chunk = ByteString.CopyFrom(fileChunk);
+
+                    await responseStream.WriteAsync(chunk).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
--- grpc_server/Startup.cs
+++ grpc_server/Startup.cs
@@ -29,6 +29,7 @@ namespace grpc_server
                 // Communication with gRPC endpoints must be made through a gRPC client.
                 // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
                 endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<TransferService>();
                 endpoints.MapGet("/",
                     async context =>
                     {
