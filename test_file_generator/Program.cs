90af034b69dc198ef5fbc5b602e5e569e005485a
Vladimir Shubarin <vhundef@pm.me>
2021-07-08 12:33:58 +0300

    Added test file generator

---
+++ test_file_generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace test_file_generator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("How many lines should file have?");
+            var lim = Convert.ToInt32(Console.ReadLine());
+            const int blockSize = 1024 * 8;
+            const int blocksPerMb = 1024 * 1024 / blockSize;
+            var data = new byte[blockSize];
+            var rng = new Random();
+            using var stream = File.OpenWrite("../../../../grpc_server/bin/Debug/net5.0/test_file.txt");
+            // There 
+            for (var i = 0; i < lim * blocksPerMb; i++)
+            {
+                rng.NextBytes(data);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
